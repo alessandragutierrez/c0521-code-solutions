e349ab8d14bcbb0625f0b3d53725abbaafd8008a
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-11 12:02:06 -0700

    updated javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,70 +1,75 @@
 var orderHistory = [
   {
-    orderDate: 'August 4, 2020',
-    total: '$34.00',
-    shipTo: 'JS Masher',
     orderNumber: '114-3941689-8772232',
-    deliveryDate: 'Aug 8, 2020',
+    orderDate: 'August 4, 2020',
+    total: 34.00,
+    recipient: 'JS Masher',
+    deliveryDate: 'August 8, 2020',
     items: [
       {
-        itemName: 'Javascript for impatient programmers',
-        data: {
-          author: 'Rauschmayer, Dr. Axel',
-          returnWindow: 'Sep 7, 2020'
-        }
+        type: 'book',
+        title: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        canReturnUntil: 'September 7, 2020',
+        price: 31.55,
+        imageUrl: 'images/js-book.png'
       }
     ]
   },
   {
-    orderDate: 'July 19, 2020',
-    total: '$44.53',
-    shipTo: 'JS Masher',
     orderNumber: '113-9984268-1280257',
-    deliveryDate: 'Jul 20, 2020',
+    orderDate: 'July 19, 2020',
+    total: 44.53,
+    recipient: 'JS Masher',
+    deliveryDate: 'July 20, 2020',
     items: [
       {
-        itemName: 'The Timeless Way of Building',
-        data: {
-          author: 'Alexander, Christopher',
-          returnWindow: 'Aug 19, 2020'
-        }
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        canReturnUntil: 'August 19, 2020',
+        price: 41.33,
+        imageUrl: 'images/timeless-book.png'
       }
     ]
   },
   {
-    orderDate: 'July 4, 2020',
-    total: '$17.22',
-    shipTo: 'JS Masher',
     orderNumber: '114-2875557-9059409',
-    deliveryDate: 'Jul 7, 2020',
+    orderDate: 'July 4, 2020',
+    total: 17.22,
+    recipient: 'JS Masher',
+    deliveryDate: 'July 7, 2020',
     items: [
       {
-        itemName: 'GameCube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-        data: {
-          returnWindow: 'Aug 5, 2020'
-        }
+        type: 'hardware',
+        name: 'GameCube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        canReturnUntil: 'August 5, 2020',
+        price: 15.98,
+        imageUrl: 'images/gamecube-controller-adapter.png'
       }
     ]
   },
   {
-    orderDate: 'July 3, 2020',
-    total: '$138.93',
-    shipTo: 'JS Masher',
     orderNumber: '113-2883177-2648248',
-    deliveryDate: 'Jul 5, 2020',
+    orderDate: 'July 3, 2020',
+    total: 138.93,
+    recipient: 'JS Masher',
+    deliveryDate: 'July 5, 2020',
     items: [
       {
-        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-        data: {
-          returnWindow: 'Aug 4, 2020'
-        }
+        type: 'hardware',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        canReturnUntil: 'August 4, 2020',
+        price: 94.95,
+        imageUrl: 'images/gamecube-controller-nintendo-switch.png'
       },
       {
-        itemName: 'The Art of Sql',
-        data: {
-          author: 'Faroult, Stephane',
-          returnWindow: 'Aug 4, 2020'
-        }
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        canReturnUntil: 'August 4, 2020',
+        price: 33.99,
+        imageUrl: 'images/art-sql.png'
       }
     ]
   }
