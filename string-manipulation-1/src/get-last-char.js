27aace3e4b9ed23a89efcd2a5c5ee4b1debe72f7
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-13 17:09:17 -0700

    updated string-manipulation-1

--- string-manipulation-1/src/get-last-char.js
+++ string-manipulation-1/src/get-last-char.js
@@ -1,9 +1,15 @@
 /* exported getLastChar */
 
+// function getLastChar(string) {
+//   var lastChar;
+//   for (var i = 0; i < string.length; i++) {
+//     lastChar = string[i];
+//   }
+//   return lastChar;
+// }
+
 function getLastChar(string) {
-  var lastChar;
-  for (var i = 0; i < string.length; i++) {
-    lastChar = string[i];
-  }
+  var lastIndex = string.length - 1;
+  var lastChar = string[lastIndex];
   return lastChar;
 }
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,13 +1,21 @@
 /* exported isVowel */
 
+// function isVowel(char) {
+//   var isVowel = false;
+//   if (char === 'a' || char === 'A' ||
+//       char === 'e' || char === 'E' ||
+//       char === 'i' || char === 'I' ||
+//       char === 'o' || char === 'O' ||
+//       char === 'u' || char === 'U') {
+//     isVowel = true;
+//   }
+//   return isVowel;
+// }
+
 function isVowel(char) {
-  var isVowel = false;
-  if (char === 'a' || char === 'A' ||
-      char === 'e' || char === 'E' ||
-      char === 'i' || char === 'I' ||
-      char === 'o' || char === 'O' ||
-      char === 'u' || char === 'U') {
-    isVowel = true;
+  var ch = char.toLowerCase();
+  if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+    return true;
   }
-  return isVowel;
+  return false;
 }
