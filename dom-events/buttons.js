da9393abdc0072915d0ed427ffe7e83d01eaeb30
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-17 13:04:06 -0700

    updated dom-events | event.target

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,8 +1,8 @@
 // click event
 function handleClick(event) {
   console.log('button clicked');
-  console.log(event);
-  console.log(clickButton);
+  console.log('event:', event);
+  console.log('event.target:', event.target);
 }
 var clickButton = document.querySelector('.click-button');
 clickButton.addEventListener('click', handleClick);
@@ -10,8 +10,8 @@ clickButton.addEventListener('click', handleClick);
 // mouseover event
 function handleMouseover(event) {
   console.log('button hovered');
-  console.log(event);
-  console.log(hoverButton);
+  console.log('event:', event);
+  console.log('event.target:', hoverButton);
 }
 var hoverButton = document.querySelector('.hover-button');
 hoverButton.addEventListener('mouseover', handleMouseover);
@@ -19,8 +19,8 @@ hoverButton.addEventListener('mouseover', handleMouseover);
 // double-click event
 function handleDoubleClick(event) {
   console.log('button double-clicked');
-  console.log(event);
-  console.log(doubleClickButton);
+  console.log('event:', event);
+  console.log('event.target:', doubleClickButton);
 }
 var doubleClickButton = document.querySelector('.double-click-button');
 doubleClickButton.addEventListener('dblclick', handleDoubleClick);
