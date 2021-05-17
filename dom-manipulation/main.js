2547001380390af2682301717f4ea297ba4350c2
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-17 15:54:58 -0700

    completed dom-manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,24 @@
+var clickNumber = 0;
+
+var hotButton = document.querySelector('.hot-button');
+var clickCount = document.querySelector('.click-count');
+
+hotButton.addEventListener('click', addClick);
+
+function addClick() {
+  clickNumber++;
+  clickCount.textContent = 'Clicks: ' + clickNumber;
+  if (clickNumber < 4) {
+    hotButton.className = 'hot-button cold';
+  } else if (clickNumber < 7) {
+    hotButton.className = 'hot-button cool';
+  } else if (clickNumber < 10) {
+    hotButton.className = 'hot-button tepid';
+  } else if (clickNumber < 13) {
+    hotButton.className = 'hot-button warm';
+  } else if (clickNumber < 16) {
+    hotButton.className = 'hot-button hot';
+  } else {
+    hotButton.className = 'hot-button nuclear';
+  }
+}
