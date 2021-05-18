305133d8861a584b0774020552eced4e9be9c0a4
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-18 11:45:14 -0700

    updated javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,5 +1,4 @@
-// create a variable to hold number of clicks, which we will use to determine
-// the off and on position. start the click count at 0.
+// create a variable to hold lightswitch boolean value of false
 
 // create a document query for the div Element
 // create a document query for the span element
@@ -7,14 +6,15 @@
 // add an event listener to the span element for a click
 
 // create a function for the event click
-//   increment the number of clicks by one
-//   create a conditional to test whether the number of clicks is even or odd
-//     if the click is odd
+//   create a conditional to test whether the switch is false(off)
+//     if the click is false(off)
+//       change the switch to true(on)
 //       turn the lightbulb on by changing the div and span classes to ON
-//     if the click is even
+//     if else
+//       change the switch to false(off)
 //       turn the lightbulb off by changing the div and span classes to OFF
 
-var numberOfClicks = 0;
+var isOn = false;
 
 var lightBulb = document.querySelector('span');
 var background = document.querySelector('div');
@@ -22,12 +22,13 @@ var background = document.querySelector('div');
 lightBulb.addEventListener('click', lightBulbSwitch);
 
 function lightBulbSwitch(event) {
-  numberOfClicks++;
-  if (numberOfClicks % 2 === 0) {
-    lightBulb.className = 'circle off';
-    background.className = 'off';
-  } else {
+  if (isOn === false) {
+    isOn = true;
     lightBulb.className = 'circle on';
     background.className = 'on';
+  } else {
+    isOn = false;
+    lightBulb.className = 'circle off';
+    background.className = 'off';
   }
 }
